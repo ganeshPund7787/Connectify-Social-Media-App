a60eeb7d38dc5235e192c50d523366a9849acc18
ganeshPund7787 <ganupund7787@gmail.com>
2024-10-22 13:12:08 -0700

    create trendsidebar

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
+import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/types";
 import React from "react";
@@ -10,13 +11,14 @@ export default async function Page() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main className="w-full min-w-0 ">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
