e06cdaee4768ac973bd760b15d740fdcbb554f70
ganeshPund7787 <ganupund7787@gmail.com>
2024-10-25 07:29:30 -0700

    add User tooltip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | bugbook",
+    template: "%s | Connectify",
     default: "bugbook",
   },
   description: "The social media app for powernerds",
