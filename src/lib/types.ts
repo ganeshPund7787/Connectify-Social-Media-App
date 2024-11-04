4bc044ee2bd021b152578ab5109426658c9e98fd
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-04 03:57:20 -0800

    get Comments API with cursor

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -69,6 +69,23 @@ export interface PostsPage {
   nextCursor: string | null;
 }
 
+export function getCommentDataInclude(loggedInUserId: string) {
+  return {
+    user: {
+      select: getUserDataSelect(loggedInUserId),
+    },
+  } satisfies Prisma.CommentInclude;
+}
+
+export type CommentData = Prisma.CommentGetPayload<{
+  include: ReturnType<typeof getCommentDataInclude>;
+}>;
+
+export interface CommentsPage {
+  comments: CommentData[];
+  previousCursor: string | null;
+}
+
 export interface FollowerInfo {
   followers: number;
   isFollowedByUser: boolean;
