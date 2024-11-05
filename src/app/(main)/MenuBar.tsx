e19f116652eab315e8113a4755f7d188c8fb4d2c
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-05 18:21:03 +0500

    complite notification feature

--- src/app/(main)/MenuBar.tsx
+++ src/app/(main)/MenuBar.tsx
@@ -1,58 +1,67 @@
+import { validateRequest } from "@/auth";
 import { Button } from "@/components/ui/button";
-import { Bell, Bookmark, Home, Mail } from "lucide-react";
+import prisma from "@/lib/prisma";
+import { Bookmark, Home, Mail } from "lucide-react";
 import Link from "next/link";
+import NotificationsButton from "./NotificationsButton";
 
 interface MenuBar {
   className?: string;
 }
 
-export default function MenuBar({ className }: MenuBar) {
+export default async function MenuBar({ className }: MenuBar) {
+  const { user } = await validateRequest();
+
+  if (!user) return null;
+
+  const unreadNotificationCount = await prisma.notification.count({
+    where: {
+      recipientId: user.id,
+      read: false,
+    },
+  });
+
   return (
-    <div className={className}>
-      <Button
-        variant={"ghost"}
-        className="flex items-center justify-start gap-3"
-        title="Home"
-        asChild
-      >
-        <Link href={"/"}>
-          <Home />
-          <span className="hidden lg:inline"> Home </span>
-        </Link>
-      </Button>
-      <Button
-        variant={"ghost"}
-        className="flex items-center justify-start gap-3"
-        title="Notification"
-        asChild
-      >
-        <Link href={"/notifications"}>
-          <Bell />
-          <span className="hidden lg:inline"> Notifications </span>
-        </Link>
-      </Button>
-      <Button
-        variant={"ghost"}
-        className="flex items-center justify-start gap-3"
-        title="Messages"
-        asChild
-      >
-        <Link href={"/messages"}>
-          <Mail />
-          <span className="hidden lg:inline"> Messages </span>
-        </Link>
-      </Button>
-      <Button
-        variant={"ghost"}
-        className="flex items-center justify-start gap-3"
-        title="Bookmarks"
-        asChild
-      >
-        <Link href={"/bookmarks"}>
-          <Bookmark />
-          <span className="hidden lg:inline"> Bookmarks </span>
-        </Link>
-      </Button>
-    </div>
+    <>
+      <div className={className}>
+        <Button
+          variant={"ghost"}
+          className="flex items-center justify-start gap-3"
+          title="Home"
+          asChild
+        >
+          <Link href={"/"}>
+            <Home />
+            <span className="hidden lg:inline"> Home </span>
+          </Link>
+        </Button>
+
+        <NotificationsButton
+          initialState={{ unreadCount: unreadNotificationCount }}
+        />
+        <Button
+          variant={"ghost"}
+          className="flex items-center justify-start gap-3"
+          title="Messages"
+          asChild
+        >
+          <Link href={"/messages"}>
+            <Mail />
+            <span className="hidden lg:inline"> Messages </span>
+          </Link>
+        </Button>
+        <Button
+          variant={"ghost"}
+          className="flex items-center justify-start gap-3"
+          title="Bookmarks"
+          asChild
+        >
+          <Link href={"/bookmarks"}>
+            <Bookmark />
+            <span className="hidden lg:inline"> Bookmarks </span>
+          </Link>
+        </Button>
+      </div>
+    </>
   );
 }
