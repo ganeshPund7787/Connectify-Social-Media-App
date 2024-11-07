64b195906593e9ff7aa141ceca752656443a4bf0
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-07 19:35:40 +0500

    working on chatting functionality

--- src/app/api/get-token/route.ts
+++ src/app/api/get-token/route.ts
@@ -5,13 +5,15 @@ export async function GET() {
   try {
     const { user } = await validateRequest();
 
+    console.log("Calling get-token for user: ", user?.id);
+
     if (!user) {
       return Response.json({ error: "Unauthorized" }, { status: 401 });
     }
-    console.log(`Calling get-token for user : `, user.id);
 
     const expirationTime = Math.floor(Date.now() / 1000) + 60 * 60;
-    const issuedAt = Math.floor(Date.now() / 1000);
+
+    const issuedAt = Math.floor(Date.now() / 1000) - 60;
 
     const token = streamServerClient.createToken(
       user.id,
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,6 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
+import streamServerClient from "@/lib/stream";
 import { createUploadthing, FileRouter } from "uploadthing/next";
 import { UploadThingError, UTApi } from "uploadthing/server";
 
@@ -31,12 +32,20 @@ export const fileRouter = {
         `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
       );
 
-      await prisma.user.update({
-        where: { id: metadata.user.id },
-        data: {
-          avatarUrl: newAvatarUrl,
-        },
-      });
+      await Promise.all([
+        prisma.user.update({
+          where: { id: metadata.user.id },
+          data: {
+            avatarUrl: newAvatarUrl,
+          },
+        }),
+        streamServerClient.partialUpdateUser({
+          id: metadata.user.id,
+          set: {
+            avatarUrl: newAvatarUrl,
+          },
+        }),
+      ]);
 
       return { avatarUrl: newAvatarUrl };
     }),
