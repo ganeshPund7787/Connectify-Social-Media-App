7b16c15f9c6f13f69f790978bdedb286b5d8c64c
ganeshPund7787 <ganupund7787@gmail.com>
2024-11-09 08:45:29 +0500

    edit btn

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -3,6 +3,7 @@
 import { SearchIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 export default function SearchField() {
   const router = useRouter();
@@ -19,9 +20,7 @@ export default function SearchField() {
     <form onSubmit={handleSubmit} method="GET" action="/search">
       <div className="relative">
         <Input name="q" placeholder="Search" className="pe-10" />
-        <button type="submit">
-          <SearchIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-muted-foreground" />
-        </button>
+        <SearchIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform cursor-pointer text-muted-foreground" />
       </div>
     </form>
   );
